9edf9057117c3c037a23741ad90692bdba6878b6
nepp-plus <nepp.plus@gmail.com>
2022-06-18 15:33:59 +0900

    로그인 화면 바인딩 예시

---
+++ app/src/main/java/com/neppplus/keepthetime_apipractice_20220618/LoginActivity.kt
@@ -0,0 +1,16 @@
+package com.neppplus.keepthetime_apipractice_20220618
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.neppplus.keepthetime_apipractice_20220618.databinding.ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityLoginBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_login)
+    }
+}
