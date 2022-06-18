d44f79df34433708c81ac59fe0ddf84b9a76d966
nepp-plus <nepp.plus@gmail.com>
2022-06-18 15:19:43 +0900

    xml에서 태그 추가 (id부여) => 자동 binding변수에 연계 확인

--- app/src/main/java/com/neppplus/keepthetime_apipractice_20220618/MainActivity.kt
+++ app/src/main/java/com/neppplus/keepthetime_apipractice_20220618/MainActivity.kt
@@ -2,6 +2,7 @@ package com.neppplus.keepthetime_apipractice_20220618
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.neppplus.keepthetime_apipractice_20220618.databinding.ActivityMainBinding
@@ -20,6 +21,12 @@ class MainActivity : AppCompatActivity() {
 //        모든 id 값들은 binding.해당id로 사용.
         binding.btnToast.setOnClickListener {
             Toast.makeText(this, "테스트", Toast.LENGTH_SHORT).show()
+        }
+
+        binding.btnLog.setOnClickListener { 
+            
+            Log.d("데이터바인딩", "로그 찍기")
+            
         }
 
     }
