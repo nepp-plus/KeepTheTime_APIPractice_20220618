1648ae386290659babd5b0b1fe0f3b36d5369644
nepp-plus <nepp.plus@gmail.com>
2022-06-18 15:18:05 +0900

    데이터 바인딩 세팅 (3)  - kt : binding 변수 정의 / 대입하기  - 모든 태그를 binding.id값으로 활용

--- app/src/main/java/com/neppplus/keepthetime_apipractice_20220618/MainActivity.kt
+++ app/src/main/java/com/neppplus/keepthetime_apipractice_20220618/MainActivity.kt
@@ -2,10 +2,25 @@ package com.neppplus.keepthetime_apipractice_20220618
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import com.neppplus.keepthetime_apipractice_20220618.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+//   (activity_main) xml의 요소를 데이터바인딩을 통해 끌어오는 binding 변수 생성.
+    lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+//        binding 변수 대입 : setContentView를 DataBindingUtil을 통해서 실행.
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+
+//        모든 id 값들은 binding.해당id로 사용.
+        binding.btnToast.setOnClickListener {
+            Toast.makeText(this, "테스트", Toast.LENGTH_SHORT).show()
+        }
+
     }
 }
