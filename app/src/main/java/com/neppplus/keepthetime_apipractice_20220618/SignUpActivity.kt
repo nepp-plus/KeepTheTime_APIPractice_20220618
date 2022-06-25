7d817531640330554848918f8f82fb9a7a060928
nepp-plus <nepp.plus@gmail.com>
2022-06-25 13:49:22 +0900

    회원가입 화면 추가  - kt 단에서 BaseActivity 상속 설정만  - mContext, apiList등의 공통 변수 자동 상속

---
+++ app/src/main/java/com/neppplus/keepthetime_apipractice_20220618/SignUpActivity.kt
@@ -0,0 +1,21 @@
+package com.neppplus.keepthetime_apipractice_20220618
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignUpActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+}
