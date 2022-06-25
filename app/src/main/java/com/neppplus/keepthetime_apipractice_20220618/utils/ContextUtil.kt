f3a723632d65b55aeff97c5f954ce39f3591af02
nepp-plus <nepp.plus@gmail.com>
2022-06-25 16:14:15 +0900

    자동 로그인 체크 여부를 저장/조회하는 기능 추가

--- app/src/main/java/com/neppplus/keepthetime_apipractice_20220618/utils/ContextUtil.kt
+++ app/src/main/java/com/neppplus/keepthetime_apipractice_20220618/utils/ContextUtil.kt
@@ -13,6 +13,7 @@ class ContextUtil {
 
 //        저장할 데이터의 항목 이름을 변수로 담아두자.
         private val LOGIN_USER_TOKEN = "LOGIN_USER_TOKEN" // 오탈자 방지용.
+        private val AUTO_LOGIN = "AUTO_LOGIN"
 
 //        실제로 저장 기능 / 조회 기능 (함수 2개)
 
@@ -30,6 +31,16 @@ class ContextUtil {
 //            적혀있는 토큰값 리턴 => 저장을 한번도 안했는데, 불러낸다면? 기본값을 뭘로 할건지. (예시 : "")
             return pref.getString( LOGIN_USER_TOKEN, "" )!!
 
+        }
+
+        fun setAutoLogin( context: Context, isAutoLogin: Boolean ) {
+            val pref = context.getSharedPreferences( prefName, Context.MODE_PRIVATE )
+            pref.edit().putBoolean( AUTO_LOGIN, isAutoLogin ).apply()
+        }
+
+        fun isAutoLogin( context: Context ) : Boolean {
+            val pref = context.getSharedPreferences( prefName, Context.MODE_PRIVATE )
+            return pref.getBoolean( AUTO_LOGIN, false )
         }
 
     }
