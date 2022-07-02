98bc2234e689479ab5d76cd01606428d39eeb2a4
nepp-plus <nepp.plus@gmail.com>
2022-07-02 16:18:26 +0900

    실제 데이터 반영 코드 (리스트뷰 작업)

--- app/src/main/java/com/neppplus/keepthetime_apipractice_20220618/adapters/SearchedUserAdapter.kt
+++ app/src/main/java/com/neppplus/keepthetime_apipractice_20220618/adapters/SearchedUserAdapter.kt
@@ -5,6 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.ImageView
+import android.widget.TextView
+import com.bumptech.glide.Glide
 import com.neppplus.keepthetime_apipractice_20220618.R
 import com.neppplus.keepthetime_apipractice_20220618.datas.UserData
 
@@ -24,6 +27,13 @@ class SearchedUserAdapter(
 
         val row = tempRow!!
 
+        val data = mList[position]
+
+        val imgProfile = row.findViewById<ImageView>(R.id.imgProfile)
+        val txtNickname = row.findViewById<TextView>(R.id.txtNickname)
+
+        txtNickname.text = data.nickname
+        Glide.with(mContext).load(data.profileImageURL).into(imgProfile)
 
         return row
     }
