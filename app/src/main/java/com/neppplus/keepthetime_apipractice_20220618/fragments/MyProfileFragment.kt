6a6c48b8d39392d3b4eefb0efd2fe5ce47562774
nepp-plus <nepp.plus@gmail.com>
2022-07-02 12:34:22 +0900

    MyProfileFragment가 BaseFragment를 활용하도록   - 의무 구현 함수 onActivityCreated 에서 실행도록

--- app/src/main/java/com/neppplus/keepthetime_apipractice_20220618/fragments/MyProfileFragment.kt
+++ app/src/main/java/com/neppplus/keepthetime_apipractice_20220618/fragments/MyProfileFragment.kt
@@ -7,7 +7,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.neppplus.keepthetime_apipractice_20220618.R
 
-class MyProfileFragment : Fragment() {
+class MyProfileFragment : BaseFragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -19,6 +19,15 @@ class MyProfileFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
 
 //        향후 동작 관련 코드 작성
 
