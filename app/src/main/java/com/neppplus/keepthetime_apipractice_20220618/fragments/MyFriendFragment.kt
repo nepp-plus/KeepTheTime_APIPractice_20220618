629cd7df1cefba5e61851162bc579e9f92050c00
nepp-plus <nepp.plus@gmail.com>
2022-07-02 14:46:07 +0900

    내 친구 목록 프래그먼트 완성

---
+++ app/src/main/java/com/neppplus/keepthetime_apipractice_20220618/fragments/MyFriendFragment.kt
@@ -0,0 +1,37 @@
+package com.neppplus.keepthetime_apipractice_20220618.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.neppplus.keepthetime_apipractice_20220618.R
+import com.neppplus.keepthetime_apipractice_20220618.databinding.FragmentMyFriendBinding
+
+class MyFriendFragment : BaseFragment() {
+
+    lateinit var binding: FragmentMyFriendBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate( inflater, R.layout.fragment_my_friend, container, false )
+        return binding.root
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+}
