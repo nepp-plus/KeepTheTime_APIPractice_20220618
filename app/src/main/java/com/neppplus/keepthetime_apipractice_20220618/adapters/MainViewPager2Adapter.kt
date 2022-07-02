27092a328c9fa5598f17f35a508d2e85dddb28fc
nepp-plus <nepp.plus@gmail.com>
2022-07-02 11:37:22 +0900

    createFragment 함수 완성  - return when 문법 재활용

--- app/src/main/java/com/neppplus/keepthetime_apipractice_20220618/adapters/MainViewPager2Adapter.kt
+++ app/src/main/java/com/neppplus/keepthetime_apipractice_20220618/adapters/MainViewPager2Adapter.kt
@@ -3,6 +3,8 @@ package com.neppplus.keepthetime_apipractice_20220618.adapters
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.neppplus.keepthetime_apipractice_20220618.fragments.MyProfileFragment
+import com.neppplus.keepthetime_apipractice_20220618.fragments.ScheduleFragment
 
 // ViewPager2 에 대응하는 어댑터 작업.
 
@@ -10,6 +12,11 @@ class MainViewPager2Adapter(fa: FragmentActivity) : FragmentStateAdapter(fa) {
     override fun getItemCount() = 2
 
     override fun createFragment(position: Int): Fragment {
+
+        return when( position ) {
+            0 -> ScheduleFragment()
+            else -> MyProfileFragment()
+        }
 
     }
 }
