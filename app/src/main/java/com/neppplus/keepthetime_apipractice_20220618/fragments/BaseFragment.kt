09bd7759af3dbe9a08aee3664a017767b4bafe6a
nepp-plus <nepp.plus@gmail.com>
2022-07-02 12:32:20 +0900

    프래그먼트도 setupEvents / setValues 함수 의무 구현 + 화면 대입시 mContext만 쓰면 되도록

--- app/src/main/java/com/neppplus/keepthetime_apipractice_20220618/fragments/BaseFragment.kt
+++ app/src/main/java/com/neppplus/keepthetime_apipractice_20220618/fragments/BaseFragment.kt
@@ -1,8 +1,22 @@
 package com.neppplus.keepthetime_apipractice_20220618.fragments
 
+import android.content.Context
+import android.os.Bundle
 import androidx.fragment.app.Fragment
 
 // 모든 프래그먼트의 공통 기능을 물려주기 위한 중간 상속 클래스
 
 abstract class BaseFragment : Fragment() {
+
+    lateinit var mContext : Context
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        mContext = requireContext() // 프래그먼트에서도 화면을 불러낼때, mContext 만 넣으면 해결
+    }
+
+    abstract fun setupEvents()
+    abstract fun setValues()
+
 }
