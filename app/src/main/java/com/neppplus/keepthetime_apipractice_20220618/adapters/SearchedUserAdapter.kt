8f04a741107523bc928dd49a8d8b875d86baf6e8
nepp-plus <nepp.plus@gmail.com>
2022-07-02 15:57:02 +0900

    getView 함수 초안 마무리

--- app/src/main/java/com/neppplus/keepthetime_apipractice_20220618/adapters/SearchedUserAdapter.kt
+++ app/src/main/java/com/neppplus/keepthetime_apipractice_20220618/adapters/SearchedUserAdapter.kt
@@ -1,7 +1,11 @@
 package com.neppplus.keepthetime_apipractice_20220618.adapters
 
 import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import com.neppplus.keepthetime_apipractice_20220618.R
 import com.neppplus.keepthetime_apipractice_20220618.datas.UserData
 
 class SearchedUserAdapter(
@@ -10,6 +14,18 @@ class SearchedUserAdapter(
     val mList: ArrayList<UserData>
 ) : ArrayAdapter<UserData>(mContext, resId, mList) {
 
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
 
+        var tempRow = convertView
+
+        if (tempRow == null) {
+            tempRow = LayoutInflater.from(mContext).inflate( R.layout.searched_user_list_item, null )
+        }
+
+        val row = tempRow!!
+
+
+        return row
+    }
 
 }
