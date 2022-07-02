55859e895c2d1e7e76b0a50f3f57bc2420055afd
nepp-plus <nepp.plus@gmail.com>
2022-07-02 12:28:53 +0900

    BaseFragment 생성 > 프래그먼트도 상속구조를 액티비티처럼 변경

---
+++ app/src/main/java/com/neppplus/keepthetime_apipractice_20220618/fragments/BaseFragment.kt
@@ -0,0 +1,8 @@
+package com.neppplus.keepthetime_apipractice_20220618.fragments
+
+import androidx.fragment.app.Fragment
+
+// 모든 프래그먼트의 공통 기능을 물려주기 위한 중간 상속 클래스
+
+abstract class BaseFragment : Fragment() {
+}
