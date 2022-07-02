2e4c51176c95f09df611e6c42c023f42291ede21
nepp-plus <nepp.plus@gmail.com>
2022-07-02 11:27:56 +0900

    일정 목록 표시용 프래그먼트 추가 (임시 UI)

---
+++ app/src/main/java/com/neppplus/keepthetime_apipractice_20220618/fragments/ScheduleFragment.kt
@@ -0,0 +1,28 @@
+package com.neppplus.keepthetime_apipractice_20220618.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.neppplus.keepthetime_apipractice_20220618.R
+
+class ScheduleFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate( R.layout.fragment_schedule, container, false )
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+//        향후 동작 관련 코드 작성
+
+    }
+
+
+}
