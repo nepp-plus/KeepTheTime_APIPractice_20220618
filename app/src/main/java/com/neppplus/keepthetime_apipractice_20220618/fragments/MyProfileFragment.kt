1f9f6da134630478203e92b617fcd74e68e8ffc4
nepp-plus <nepp.plus@gmail.com>
2022-07-02 12:42:18 +0900

    프래그먼트 xml태그 > kt단에서 활용  - 프래그먼트의 데이터 바인딩 세팅 필요

--- app/src/main/java/com/neppplus/keepthetime_apipractice_20220618/fragments/MyProfileFragment.kt
+++ app/src/main/java/com/neppplus/keepthetime_apipractice_20220618/fragments/MyProfileFragment.kt
@@ -6,6 +6,11 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.neppplus.keepthetime_apipractice_20220618.R
+import com.neppplus.keepthetime_apipractice_20220618.datas.BasicResponse
+import com.neppplus.keepthetime_apipractice_20220618.utils.ContextUtil
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MyProfileFragment : BaseFragment() {
 
@@ -33,9 +38,28 @@ class MyProfileFragment : BaseFragment() {
 
 //        1. 서버에 내 정보 요청 (GET - /user) 함수 사용 => MyProfileFragment > BaseFragment() > Fragment() 상속
 
-        apiList.getRequestMyInfo()
+        apiList.getRequestMyInfo( ContextUtil.getLoginUserToken(mContext) ).enqueue(object : Callback<BasicResponse> {
+            override fun onResponse(call: Call<BasicResponse>, response: Response<BasicResponse>) {
 
-//        2. 서버의 응답 분석 => 프사 / 닉네임 / 이메일 반영
+                if (response.isSuccessful) {
+
+
+//                   2. 서버의 응답 분석 => 프사 / 닉네임 / 이메일 반영
+
+                    val br = response.body()!!
+
+//                    프래그먼트도 데이터바인딩 세팅을 해야, 화면의 id 붙여둔 태그들을 가져다 사용 가능.
+
+                }
+            }
+
+            override fun onFailure(call: Call<BasicResponse>, t: Throwable) {
+
+            }
+
+        })
+
+
 
     }
 
