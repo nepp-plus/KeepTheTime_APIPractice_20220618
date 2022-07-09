e03935dddcbc64aa6254b9c92a4d0ca46830df2b
nepp-plus <nepp.plus@gmail.com>
2022-07-09 12:17:22 +0900

    검색 버튼 누르면 => 서버에 검색 API 호출  - 리싸이클러뷰의 목록을 채우는 관점

--- app/src/main/java/com/neppplus/keepthetime_apipractice_20220618/AddFriendRecyclerViewActivity.kt
+++ app/src/main/java/com/neppplus/keepthetime_apipractice_20220618/AddFriendRecyclerViewActivity.kt
@@ -4,10 +4,19 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import com.neppplus.keepthetime_apipractice_20220618.databinding.ActivityAddFriendRecyclerViewBinding
+import com.neppplus.keepthetime_apipractice_20220618.datas.BasicResponse
+import com.neppplus.keepthetime_apipractice_20220618.datas.UserData
+import com.neppplus.keepthetime_apipractice_20220618.utils.ContextUtil
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class AddFriendRecyclerViewActivity : BaseActivity() {
 
     lateinit var binding: ActivityAddFriendRecyclerViewBinding
+
+//    검색 결과 나온 사람들을 담을 목록 변수
+    val mSearchedUserList = ArrayList<UserData>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,6 +26,39 @@ class AddFriendRecyclerViewActivity : BaseActivity() {
     }
 
     override fun setupEvents() {
+
+//        검색 버튼이 눌리면 => 서버에서 사용자 목록을 검색해서 => 그 결과를 ArrayList에 add
+
+        binding.btnSearch.setOnClickListener {
+
+            val inputKeyword = binding.edtNickname.text.toString()
+
+            apiList.getRequestSearchUser(
+                ContextUtil.getLoginUserToken(mContext),
+                inputKeyword
+            ).enqueue( object : Callback<BasicResponse> {
+                override fun onResponse(
+                    call: Call<BasicResponse>,
+                    response: Response<BasicResponse>
+                ) {
+
+                    if (response.isSuccessful) {
+
+//                        검색이 성공적으로 이뤄졌다면
+//                        서버가 내려준 목록을 => ArrayList에 추가 => 리싸이클러뷰의 내용물로 반영
+
+                    }
+
+                }
+
+                override fun onFailure(call: Call<BasicResponse>, t: Throwable) {
+
+                }
+
+            } )
+
+
+        }
 
     }
 
