78ac7005265af2b0ae3a8837ad14af1a216d2aa5
nepp-plus <nepp.plus@gmail.com>
2022-07-09 12:03:10 +0900

    리싸이클러뷰 어댑터 생성  - ViewHolder 제작 / 자료형 설정

---
+++ app/src/main/java/com/neppplus/keepthetime_apipractice_20220618/adapters/SearchedUserRecyclerAdapter.kt
@@ -0,0 +1,12 @@
+package com.neppplus.keepthetime_apipractice_20220618.adapters
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class SearchedUserRecyclerAdapter : RecyclerView.Adapter<SearchedUserRecyclerAdapter.MyViewHolder>() {
+
+    inner class MyViewHolder(val row: View) : RecyclerView.ViewHolder(row) {
+
+    }
+
+}
