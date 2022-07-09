79f9275864a79dc4f49f422e5cc774690269ee9a
nepp-plus <nepp.plus@gmail.com>
2022-07-09 12:07:21 +0900

    리싸이클러뷰 어댑터 뼈대작업 마무리

--- app/src/main/java/com/neppplus/keepthetime_apipractice_20220618/adapters/SearchedUserRecyclerAdapter.kt
+++ app/src/main/java/com/neppplus/keepthetime_apipractice_20220618/adapters/SearchedUserRecyclerAdapter.kt
@@ -1,12 +1,32 @@
 package com.neppplus.keepthetime_apipractice_20220618.adapters
 
+import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.neppplus.keepthetime_apipractice_20220618.R
+import com.neppplus.keepthetime_apipractice_20220618.datas.UserData
 
-class SearchedUserRecyclerAdapter : RecyclerView.Adapter<SearchedUserRecyclerAdapter.MyViewHolder>() {
+class SearchedUserRecyclerAdapter(
+    val mContext: Context,
+    val mList: ArrayList<UserData>
+) : RecyclerView.Adapter<SearchedUserRecyclerAdapter.MyViewHolder>() {
 
     inner class MyViewHolder(val row: View) : RecyclerView.ViewHolder(row) {
 
     }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+
+        val row = LayoutInflater.from(mContext).inflate( R.layout.searched_user_list_item, parent, false )
+        return MyViewHolder(row)
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+
+    }
+
+    override fun getItemCount() = mList.size
 
 }
