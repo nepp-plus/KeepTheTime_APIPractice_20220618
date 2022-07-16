3fe443d2c3f536697ceff7060997a2e9f31d5240
nepp-plus <nepp.plus@gmail.com>
2022-07-16 11:08:20 +0900

    Calendar를 활용한 선택 일시 변수  - DatePicker 띄울 때 기본값으로 활용

--- app/src/main/java/com/neppplus/keepthetime_apipractice_20220618/EditScheduleActivity.kt
+++ app/src/main/java/com/neppplus/keepthetime_apipractice_20220618/EditScheduleActivity.kt
@@ -6,10 +6,14 @@ import android.os.Bundle
 import android.widget.DatePicker
 import androidx.databinding.DataBindingUtil
 import com.neppplus.keepthetime_apipractice_20220618.databinding.ActivityEditScheduleBinding
+import java.util.*
 
 class EditScheduleActivity : BaseActivity() {
 
     lateinit var binding: ActivityEditScheduleBinding
+
+//    선택한 약속 일시를 저장할 캘린더 변수.
+    val mSelectedDateTime = Calendar.getInstance() // 현재 일시가 자동 저장
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,12 +36,14 @@ class EditScheduleActivity : BaseActivity() {
                 }
             }
 
+//            선택된 일시 (기본 - 현재 일시) 를 DatePicker의 기본값으로 설정
+
             val dialog = DatePickerDialog(
                 mContext,
                 dateSetListener,
-                2022,
-                7,
-                16
+                mSelectedDateTime.get(Calendar.YEAR),
+                mSelectedDateTime.get(Calendar.MONTH),
+                mSelectedDateTime.get(Calendar.DAY_OF_MONTH)
             ).show()
 
         }
