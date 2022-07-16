6aee75e3b2eb8ea30461562c2c542e2c68e412e7
nepp-plus <nepp.plus@gmail.com>
2022-07-16 10:44:14 +0900

    날짜 선택 달력 팝업 (DatePickerDialog) 띄우고 선택값 반영  - 시작날짜 / 선택된 날짜 모두 이상 동작

--- app/src/main/java/com/neppplus/keepthetime_apipractice_20220618/EditScheduleActivity.kt
+++ app/src/main/java/com/neppplus/keepthetime_apipractice_20220618/EditScheduleActivity.kt
@@ -1,7 +1,9 @@
 package com.neppplus.keepthetime_apipractice_20220618
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.DatePicker
 import androidx.databinding.DataBindingUtil
 import com.neppplus.keepthetime_apipractice_20220618.databinding.ActivityEditScheduleBinding
 
@@ -17,6 +19,28 @@ class EditScheduleActivity : BaseActivity() {
     }
 
     override fun setupEvents() {
+
+        binding.txtDate.setOnClickListener {
+
+            val dateSetListener = object : DatePickerDialog.OnDateSetListener {
+                override fun onDateSet(p0: DatePicker?, year: Int, month: Int, dayOfMonth: Int) {
+
+//                    임시 - txtDate에 반영
+
+                    binding.txtDate.text = "${month}월 ${dayOfMonth}일"
+
+                }
+            }
+
+            val dialog = DatePickerDialog(
+                mContext,
+                dateSetListener,
+                2022,
+                7,
+                16
+            ).show()
+
+        }
 
     }
 
