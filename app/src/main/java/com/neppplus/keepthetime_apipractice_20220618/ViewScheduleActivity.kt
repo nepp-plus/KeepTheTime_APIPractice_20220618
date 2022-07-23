bc875ac11822361e3afe27bd5fcfcbaa8b176ddd
nepp-plus <nepp.plus@gmail.com>
2022-07-23 14:01:55 +0900

    약속 조회 화면 > 문구 데이터만 먼저 세팅

--- app/src/main/java/com/neppplus/keepthetime_apipractice_20220618/ViewScheduleActivity.kt
+++ app/src/main/java/com/neppplus/keepthetime_apipractice_20220618/ViewScheduleActivity.kt
@@ -4,14 +4,19 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import com.neppplus.keepthetime_apipractice_20220618.databinding.ActivityViewScheduleBinding
+import com.neppplus.keepthetime_apipractice_20220618.datas.AppointmentData
 
 class ViewScheduleActivity : BaseActivity() {
 
     lateinit var binding: ActivityViewScheduleBinding
 
+//    이 화면에 들고 들어온 약속 데이터
+    lateinit var mAppointmentData: AppointmentData
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_view_schedule)
+        mAppointmentData = intent.getSerializableExtra("schedule") as AppointmentData
         setupEvents()
         setValues()
     }
@@ -21,6 +26,9 @@ class ViewScheduleActivity : BaseActivity() {
     }
 
     override fun setValues() {
+
+        binding.txtTitle.text = mAppointmentData.title
+        binding.txtDateTime.text = mAppointmentData.datetime
 
     }
 }
