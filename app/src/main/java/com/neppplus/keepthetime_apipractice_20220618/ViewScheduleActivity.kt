5ffa31a2d1b3069b322ba1b88d7908fdd74706a9
nepp-plus <nepp.plus@gmail.com>
2022-07-23 13:55:19 +0900

    스케쥴 조회 화면에서도 데이터바인딩 / BaseActivity 세팅 진행

--- app/src/main/java/com/neppplus/keepthetime_apipractice_20220618/ViewScheduleActivity.kt
+++ app/src/main/java/com/neppplus/keepthetime_apipractice_20220618/ViewScheduleActivity.kt
@@ -2,10 +2,25 @@ package com.neppplus.keepthetime_apipractice_20220618
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.neppplus.keepthetime_apipractice_20220618.databinding.ActivityViewScheduleBinding
 
-class ViewScheduleActivity : AppCompatActivity() {
+class ViewScheduleActivity : BaseActivity() {
+
+    lateinit var binding: ActivityViewScheduleBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_view_schedule)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_view_schedule)
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
     }
 }
