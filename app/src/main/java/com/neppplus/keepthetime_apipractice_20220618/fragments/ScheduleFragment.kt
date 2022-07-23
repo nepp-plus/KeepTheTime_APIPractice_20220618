86e90521f77f5a3b1ac8044908f09b641966663c
nepp-plus <nepp.plus@gmail.com>
2022-07-23 11:10:08 +0900

    어댑터 생성 / 리싸이클러뷰 연결  - 비동기 방식 동작의 설명

--- app/src/main/java/com/neppplus/keepthetime_apipractice_20220618/fragments/ScheduleFragment.kt
+++ app/src/main/java/com/neppplus/keepthetime_apipractice_20220618/fragments/ScheduleFragment.kt
@@ -7,8 +7,10 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.neppplus.keepthetime_apipractice_20220618.EditScheduleActivity
 import com.neppplus.keepthetime_apipractice_20220618.R
+import com.neppplus.keepthetime_apipractice_20220618.adapters.MyScheduleAdapter
 import com.neppplus.keepthetime_apipractice_20220618.databinding.FragmentScheduleBinding
 import com.neppplus.keepthetime_apipractice_20220618.datas.AppointmentData
 import com.neppplus.keepthetime_apipractice_20220618.datas.BasicResponse
@@ -21,6 +23,8 @@ class ScheduleFragment : BaseFragment() {
     lateinit var binding: FragmentScheduleBinding
 
     val mScheduleList = ArrayList<AppointmentData>()
+
+    lateinit var mAdapter: MyScheduleAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -49,7 +53,14 @@ class ScheduleFragment : BaseFragment() {
     }
 
     override fun setValues() {
+//        먼저 서버로 데이터를 가지러 가자 => 먼 길을 가야함
         getMySchedulesFromServer()
+
+//        그 후에 어댑터 세팅 => 가까이에서 해결 가능. 늦게 시작하지만, 먼저 마무리 됨.
+        mAdapter = MyScheduleAdapter( mContext, mScheduleList )
+        binding.myScheduleRecyclerView.adapter = mAdapter
+        binding.myScheduleRecyclerView.layoutManager = LinearLayoutManager(mContext)
+
     }
 
     fun getMySchedulesFromServer() {
@@ -64,7 +75,11 @@ class ScheduleFragment : BaseFragment() {
 
                     val br = response.body()!!
 
+//                    상황에 따라, 어댑터 세팅이 먼저 끝나고 / 나중에 데이터가 추가되는 경우도 있다.
                     mScheduleList.addAll( br.data.appointments )
+
+//                    어댑터가 사용하는 ArrayList의 내용물 변경 => 어댑터도 새로 고침
+                    mAdapter.notifyDataSetChanged()
 
 
                 }
