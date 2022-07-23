577c0400785d412855175fa7e4830efa0424bef5
nepp-plus <nepp.plus@gmail.com>
2022-07-23 10:44:44 +0900

    리싸이클러 어댑터 - 필수 작업 3가지 함수 중 2가지 작업 완료 (나머지는 차후 작업)

--- app/src/main/java/com/neppplus/keepthetime_apipractice_20220618/adapters/MyScheduleAdapter.kt
+++ app/src/main/java/com/neppplus/keepthetime_apipractice_20220618/adapters/MyScheduleAdapter.kt
@@ -1,12 +1,33 @@
 package com.neppplus.keepthetime_apipractice_20220618.adapters
 
+import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.neppplus.keepthetime_apipractice_20220618.R
+import com.neppplus.keepthetime_apipractice_20220618.datas.AppointmentData
 
-class MyScheduleAdapter : RecyclerView.Adapter< MyScheduleAdapter.MyViewHolder >() {
+class MyScheduleAdapter(
+    val mContext: Context,
+    val mList: ArrayList< AppointmentData >
+) : RecyclerView.Adapter< MyScheduleAdapter.MyViewHolder >() {
 
     inner class MyViewHolder(val row: View) : RecyclerView.ViewHolder(row) {
 
     }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+
+        val row = LayoutInflater.from(mContext).inflate( R.layout.my_schedule_list_item, parent, false )
+        return MyViewHolder(row)
+
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+
+    }
+
+    override fun getItemCount() = mList.size
 
 }
