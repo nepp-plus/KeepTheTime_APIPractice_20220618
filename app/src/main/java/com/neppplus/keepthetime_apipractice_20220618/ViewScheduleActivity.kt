fa2733b2627153bec5d0583aac536c2d4d711295
nepp-plus <nepp.plus@gmail.com>
2022-07-23 14:09:00 +0900

    네이버 지도 로딩 완료시, 카메라를 해당 약속 장소로 이동

--- app/src/main/java/com/neppplus/keepthetime_apipractice_20220618/ViewScheduleActivity.kt
+++ app/src/main/java/com/neppplus/keepthetime_apipractice_20220618/ViewScheduleActivity.kt
@@ -3,6 +3,9 @@ package com.neppplus.keepthetime_apipractice_20220618
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import com.naver.maps.geometry.LatLng
+import com.naver.maps.map.CameraUpdate
+import com.naver.maps.map.NaverMap
 import com.neppplus.keepthetime_apipractice_20220618.databinding.ActivityViewScheduleBinding
 import com.neppplus.keepthetime_apipractice_20220618.datas.AppointmentData
 
@@ -12,6 +15,10 @@ class ViewScheduleActivity : BaseActivity() {
 
 //    이 화면에 들고 들어온 약속 데이터
     lateinit var mAppointmentData: AppointmentData
+
+//    로딩이 완료된 실제 지도 객체
+
+    lateinit var naverMap: NaverMap
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,6 +36,23 @@ class ViewScheduleActivity : BaseActivity() {
 
         binding.txtTitle.text = mAppointmentData.title
         binding.txtDateTime.text = mAppointmentData.datetime
+        
+        binding.mapView.getMapAsync { 
+            naverMap = it
+
+//            약속장소 좌표 변수로 저장
+            val latLng = LatLng( mAppointmentData.latitude, mAppointmentData.longitude )
+
+//            카메라를 약속 장소로 이동
+
+            val cameraUpdate = CameraUpdate.scrollTo( latLng )
+
+            naverMap.moveCamera( cameraUpdate )
+
+            
+//            마커를 약속 장소에 표시
+            
+        }
 
     }
 }
